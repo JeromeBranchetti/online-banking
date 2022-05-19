59f5813711177697a1a273fb978c79fba8352636
brngpp <giuseppe.bruniani@gmail.com>
2022-05-19 14:29:48 +0200

    approuting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { HomeComponent } from './home/home.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserDashBoardComponent } from './user-dash-board/user-dash-board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'',component:HomeComponent
+},
+   {
+     path:'login', component:LoginComponent
+     
+   },
+  {  
+    path:'userDashBoard',component:UserDashBoardComponent
+   },
+ {
+  path:'signUp',component:SignUpComponent
+   }
+ ]
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

c11ec266a10c1b6cdb2dfc2634319007787d55a5
ChristianKareemCianci <ckcianci@gmail.com>
2022-05-19 14:53:00 +0200

    Modify payment component

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+  styleUrls: ['./payment.component.css'],
 })
 export class PaymentComponent implements OnInit {
+  bankTransferChoice: boolean = false;
+  phoneTopUp: boolean = false;
 
-  constructor() { }
+  bankTransferForm = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    lastName: new FormControl(null, Validators.required),
+    iban: new FormControl(null, Validators.required),
+    amount: new FormControl(null, Validators.required),
+  });
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onBankTransfer() {
+    this.bankTransferChoice = true;
+    this.phoneTopUp = false;
   }
 
+  onPhoneTopUp() {
+    this.bankTransferChoice = false;
+    this.phoneTopUp = true;
+  }
 }

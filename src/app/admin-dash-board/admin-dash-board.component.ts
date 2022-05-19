fb007189586a6f0b794bb2269a9789944612d672
ChristianKareemCianci <ckcianci@gmail.com>
2022-05-19 17:50:13 +0200

    modify adminDashboard

--- src/app/admin-dash-board/admin-dash-board.component.ts
+++ src/app/admin-dash-board/admin-dash-board.component.ts
@@ -1,15 +1,23 @@
+import { RequestModel } from './request.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-dash-board',
   templateUrl: './admin-dash-board.component.html',
-  styleUrls: ['./admin-dash-board.component.css']
+  styleUrls: ['./admin-dash-board.component.css'],
 })
 export class AdminDashBoardComponent implements OnInit {
+  requests: RequestModel[] = [
+    {
+      name: 'Andrea',
+      lastName: 'Rossi',
+      iban: '1234',
+      dateOfBirth: '111',
+      email: 'andrea@rossi.it',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/admin-dash-board/request.model.ts
@@ -0,0 +1,7 @@
+export class RequestModel {
+  name: string;
+  lastName: string;
+  iban: string;
+  dateOfBirth: string;
+  email: string;
+}

ef9c0812fc8d4925ebf9265973198d68454a9a36
Jerome Branchetti <93258692+JeromeBranchetti@users.noreply.github.com>
2022-05-20 22:39:00 +0200

    routing

--- src/app/foot-bar/foot-bar.component.ts
+++ src/app/foot-bar/foot-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-foot-bar',
@@ -7,13 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FootBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  onDashBoard() {}
-  onPayment() {}
-  onMovementDetail() {}
+  onDashBoard() {
+    this.router.navigate(['/userDashboard']);
+  }
+  onPayment() {
+    this.router.navigate(['/payment']);
+  }
+  onMovementDetail() {
+    this.router.navigate(['/account-detail']);
+  }
 
 }

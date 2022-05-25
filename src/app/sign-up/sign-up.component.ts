261776e2d5b65a654d43278ba051af91448b39f7
brngpp <giuseppe.bruniani@gmail.com>
2022-05-25 17:21:26 +0200

    add route

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,7 +1,7 @@
 import { Router } from '@angular/router';
 import { SignUpService } from './../service/SignUpService';
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -11,13 +11,13 @@ import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from
 export class SignUpComponent implements OnInit {
 
   constructor( public SUService:SignUpService,private router:Router) { }
-  regex = new RegExp('^[A-Z][a-z]{4,}[0-9]*[^W]$')
+  regex = new RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^W]).{8,32}$')
   ngOnInit(): void {
   }
   signUp_form=new FormGroup(
     {
       name: new FormControl(null,Validators.required,),
-      date:new FormControl(null,[Validators.required,this.dataValidator]),
+      date:new FormControl(null,[Validators.required]),
       last_name:new FormControl(null,Validators.required),
       email:new FormControl(null,[Validators.required,Validators.email]),
       password:new FormControl(null,[Validators.required,Validators.pattern(this.regex)])
@@ -26,7 +26,8 @@ export class SignUpComponent implements OnInit {
 
     }
   );
-  dataValidator():ValidatorFn {return (c: AbstractControl): { [key: string]: boolean } | null =>{
+  dataValidator(c: AbstractControl): { [key: string]: boolean } | null {
+    console.log("test")
    
     
     let birth=new Date(c.value)
@@ -34,36 +35,36 @@ export class SignUpComponent implements OnInit {
     
     if((today.getFullYear()-birth.getFullYear())>18){
      
-       return {["maggiorenne"]:true};
+       return {maggiorenne:true};
     }
     else if((today.getFullYear()-birth.getFullYear())<18){
-      return {["maggiorenne"]:false};
+      return {'maggiorenne':false};
     }
      else if((today.getFullYear()-birth.getFullYear())===18){
          if((today.getMonth()-birth.getMonth())>0){
          
-           return {["maggiorenne"]:true};
+           return {'maggiorenne':true};
          }
          else if((today.getMonth()-birth.getMonth())<0){
-           return {["maggiorenne"]:false};
+           return {'maggiorenne':false};
          }
          else if((today.getMonth()-birth.getMonth())===0){
         if(today.getDay()-birth.getDay()>0){
-             return {["maggiorenne"]:false};
+             return {'maggiorenne':false};
            }
-           else if(today.getDay()-birth.getDay()<0){
-            return {["maggiorenne"]:false};
+           else if((today.getDay()-birth.getDay())<0){
+            return {'maggiorenne':false};
            }
             else if(today.getDay()===birth.getDay()){
             
-        return {["maggiorenne"]:true};
+        return {'maggiorenne':true};
             }
          }
      }
     
      return null;
 }
-  }
+  
 
     delete(){
       

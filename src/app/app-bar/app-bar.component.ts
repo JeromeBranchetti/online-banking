cb582a6270504d6a625a4ab9aac9314d3aa8ee20
ChristianKareemCianci <ckcianci@gmail.com>
2022-05-27 15:25:29 +0200

    changes

--- src/app/app-bar/app-bar.component.ts
+++ src/app/app-bar/app-bar.component.ts
@@ -1,36 +1,31 @@
-
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { SpioneService } from '../service/SpioneService';
-
+import { SpioneService } from '../service/spione.service';
 
 @Component({
   selector: 'app-bar',
   templateUrl: './app-bar.component.html',
-  styleUrls: ['./app-bar.component.css']
+  styleUrls: ['./app-bar.component.css'],
 })
 export class AppBarComponent implements OnInit {
-  eyeOpen:boolean = true;
+  eyeOpen: boolean = true;
 
-
-  constructor(private S:SpioneService,private router: Router) { }
+  constructor(private S: SpioneService, private router: Router) {}
 
   ngOnInit(): void {
-    this.S.SpioneMode(this.eyeOpen)
+    this.S.SpioneMode(this.eyeOpen);
   }
 
   openCloseToggle() {
     this.eyeOpen = !this.eyeOpen;
-    this.S.SpioneMode(this.eyeOpen)
-    
+    this.S.SpioneMode(this.eyeOpen);
   }
 
   logOut() {
     this.router.navigate(['/']);
   }
-  
+
   toHome() {
     this.router.navigate(['/']);
   }
-
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,88 +1,70 @@
+import { SignUpService } from '../service/SignUp.service';
 import { Router } from '@angular/router';
-import { SignUpService } from './../service/SignUpService';
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  constructor(public SUService: SignUpService, private router: Router) {}
+  regex = new RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^W]).{8,32}$');
 
-  constructor( public SUService:SignUpService,private router:Router) { }
-  regex = new RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^W]).{8,32}$')
+  ngOnInit(): void {}
+  signUp_form = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    date: new FormControl(null, [Validators.required]),
+    last_name: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(this.regex),
+    ]),
+  });
 
-  ngOnInit(): void {
-  }
-  signUp_form=new FormGroup(
-    {
-      name: new FormControl(null,Validators.required,),
-      date:new FormControl(null,[Validators.required]),
-      last_name:new FormControl(null,Validators.required),
-      email:new FormControl(null,[Validators.required,Validators.email]),
-      password:new FormControl(null,[Validators.required,Validators.pattern(this.regex)])
-       
+  controlDate(c: AbstractControl) {
+    let birth = new Date(c.value);
+    let today = new Date();
 
-
-    }
-  );
-  
-   
-    controlDate(c:AbstractControl){
-    let birth=new Date(c.value)
-    let today=new Date()
-    
-    if((today.getFullYear()-birth.getFullYear())>18){
-     
-       return {maggiorenne:true};
-    }
-    else if((today.getFullYear()-birth.getFullYear())<18){
+    if (today.getFullYear() - birth.getFullYear() > 18) {
+      return { maggiorenne: true };
+    } else if (today.getFullYear() - birth.getFullYear() < 18) {
       return false;
-    }
-     else if((today.getFullYear()-birth.getFullYear())===18){
-         if((today.getMonth()-birth.getMonth())>0){
-         
-           return true;
-         }
-         else if((today.getMonth()-birth.getMonth())<0){
-           return false;
-         }
-         else if((today.getMonth()-birth.getMonth())===0){
-        if(today.getDay()-birth.getDay()>0){
-             return false;
-           }
-           else if((today.getDay()-birth.getDay())<0){
-            return false;
-           }
-            else if(today.getDay()===birth.getDay()){
-            
+    } else if (today.getFullYear() - birth.getFullYear() === 18) {
+      if (today.getMonth() - birth.getMonth() > 0) {
         return true;
-            }
-         }
-     }
-    
-     return null;
-    }
-  
-
-    delete(){
-      
-      this.signUp_form.reset()
-      
-    }
-    signUp(){
-    if(this.controlDate(this.signUp_form.get("date"))){
-
-    
-     this.SUService.newUtente(this.signUp_form);
-     this.router.navigate(['userDashboard']);
-
-    }
-    else{
-      alert("non sei maggiorenne")
+      } else if (today.getMonth() - birth.getMonth() < 0) {
+        return false;
+      } else if (today.getMonth() - birth.getMonth() === 0) {
+        if (today.getDay() - birth.getDay() > 0) {
+          return false;
+        } else if (today.getDay() - birth.getDay() < 0) {
+          return false;
+        } else if (today.getDay() === birth.getDay()) {
+          return true;
+        }
+      }
     }
 
+    return null;
+  }
+
+  delete() {
+    this.signUp_form.reset();
+  }
+  signUp() {
+    if (this.controlDate(this.signUp_form.get('date'))) {
+      this.SUService.newUtente(this.signUp_form);
+      this.router.navigate(['userDashboard']);
+    } else {
+      alert('non sei maggiorenne');
     }
-    
+  }
 }

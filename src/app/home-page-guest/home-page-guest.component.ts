b57ee5942ed33130d09163f95db527f9b20fc5ad
Jerome Branchetti <93258692+JeromeBranchetti@users.noreply.github.com>
2022-06-01 17:02:36 +0200

    home page responsive

--- src/app/home-page-guest/home-page-guest.component.ts
+++ src/app/home-page-guest/home-page-guest.component.ts
@@ -6,22 +6,26 @@ import { conto } from '../class/conto';
 @Component({
   selector: 'app-home-page-guest',
   templateUrl: './home-page-guest.component.html',
-  styleUrls: ['./home-page-guest.component.css']
+  styleUrls: ['./home-page-guest.component.css'],
 })
 export class HomePageGuestComponent implements OnInit {
-  conti:conto[]=[]
 
-  constructor(private router: Router , private Sus:SignUpService) { }
+  conti: conto[] = [
+    new conto(50000),
+    new conto(1000000),
+    new conto(999999)
+    
+  ];
+
+  constructor(private router: Router, private Sus: SignUpService) {}
 
   ngOnInit(): void {
-    this.Sus.bsconto.subscribe((res)=>{
-      this.conti=res;
-    })
-
+    this.Sus.bsconto.subscribe((res) => {
+      this.conti = res;
+    });
   }
 
   toPayment() {
-    this.router.navigate(['/payment'])
+    this.router.navigate(['/payment']);
   }
-
 }

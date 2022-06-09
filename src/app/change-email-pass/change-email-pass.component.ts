63ff68bc7ee2f761ec5cd83020faa9d9cbc1d4a7
Jerome Branchetti <93258692+JeromeBranchetti@users.noreply.github.com>
2022-06-09 12:41:35 +0200

    error page navigation

--- src/app/change-email-pass/change-email-pass.component.ts
+++ src/app/change-email-pass/change-email-pass.component.ts
@@ -3,15 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-change-email-pass',
   templateUrl: './change-email-pass.component.html',
-  styleUrls: ['./change-email-pass.component.css']
+  styleUrls: ['./change-email-pass.component.css'],
 })
 export class ChangeEmailPassComponent implements OnInit {
   isEmail: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   switchEmailPass() {
     this.isEmail = !this.isEmail;
@@ -23,7 +22,5 @@ export class ChangeEmailPassComponent implements OnInit {
 
   onChangeEmail() {
     //fare logica richiesta HTTP modifica email
-    
   }
-
 }

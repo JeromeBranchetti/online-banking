77ddcdaa7f05973068ef58870c5b6ddbcc1b29a5
brngpp <giuseppe.bruniani@gmail.com>
2022-06-10 10:46:17 +0200

    commit

--- src/app/service/httpRequest.service.ts
+++ src/app/service/httpRequest.service.ts
@@ -104,6 +104,7 @@ export class HttpRequestService {
             //   queryParams: {
             //     idUt: this.US.idUt,
             //     idCont: this.US.idCont,
+            //zanzan
             //   },
             // });
           });
